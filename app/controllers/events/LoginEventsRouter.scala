97f633bfa755b5863d1b476ed408e648e75b3c69
Queengamer <33478834+Queengamer@users.noreply.github.com>
2018-09-04 08:36:07 +0200

    Update LoginEventsRouter.scala

--- app/controllers/events/LoginEventsRouter.scala
+++ app/controllers/events/LoginEventsRouter.scala
@@ -1,6 +1,18 @@
+package controllers.events
+
+import javax.inject.Inject
+import play.api.routing.Router.Routes
+import play.api.routing.SimpleRouter
 
 /**
   *Creater Natasha
   *Reviewer Omega
   *
   */
+
+class LoginEventsRouter @Inject()
+ (loginEventsCreationRouter: LoginEventsController)
+  extends SimpleRouter {
+  override def routes: Routes = ???
+}
+
