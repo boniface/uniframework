a2ad58ffde299b301c135f71ce1feaac55c72ef6
Queengamer <33478834+Queengamer@users.noreply.github.com>
2018-09-04 08:29:50 +0200

    Update LoginEventsController.scala

--- app/controllers/events/LoginEventsController.scala
+++ app/controllers/events/LoginEventsController.scala
@@ -1,6 +1,15 @@
+package controllers.events
+
+import javax.inject.Inject
+import play.api.mvc.{AbstractController, ControllerComponents}
 
 /**
   *Creater Natasha
   *Reviewer Omega
   *
   */
+
+class LoginEventsController @Inject()
+(cc: ControllerComponents) extends AbstractController(cc) {
+}
+
