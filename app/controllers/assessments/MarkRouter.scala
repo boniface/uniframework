3b576a5149eeecf08ecbbf2c2c4a50589c98082a
Kessel <33414897+Koumou@users.noreply.github.com>
2018-09-05 16:38:06 +0200

    Update MarkRouter.scala

--- app/controllers/assessments/MarkRouter.scala
+++ app/controllers/assessments/MarkRouter.scala
@@ -3,3 +3,16 @@
   *Reviewer Dorcas
   *
   */
+
+package controllers.assessments
+
+import javax.inject.Inject
+import play.api.routing.Router.Routes
+import play.api.routing.SimpleRouter
+
+
+class MarkRouter @Inject()
+ (markCreationRouter: MarkController)
+  extends SimpleRouter {
+  override def routes: Routes = ???
+}
