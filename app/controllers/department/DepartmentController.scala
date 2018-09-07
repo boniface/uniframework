49f82d087ccf681cd1a117d07e14ab0834e1f980
Yandisa <38737545+yandisakat@users.noreply.github.com>
2018-09-07 21:40:49 +0200

    Update DepartmentController.scala

--- app/controllers/department/DepartmentController.scala
+++ app/controllers/department/DepartmentController.scala
@@ -1,5 +1,10 @@
 package controllers.department
 
+/*
+ * Created by Reece
+ * Reviewed by Yandisa
+ */
+
 import javax.inject.Inject
 import play.api.mvc.{AbstractController, ControllerComponents}
 
