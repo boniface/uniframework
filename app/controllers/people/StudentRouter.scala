6f3172f28bdcf9da796c0bb60cda2d67c96cb507
Kessel <33414897+Koumou@users.noreply.github.com>
2018-09-07 13:09:59 +0200

    Update StudentRouter.scala

--- app/controllers/people/StudentRouter.scala
+++ app/controllers/people/StudentRouter.scala
@@ -3,3 +3,15 @@
   *Reviewer Reece
   *
   */
+package controllers.people
+
+import javax.inject.Inject
+import play.api.routing.Router.Routes
+import play.api.routing.SimpleRouter
+
+
+class StudentRouter @Inject()
+ (studentCreationRouter: StudentController)
+  extends SimpleRouter {
+  override def routes: Routes = ???
+}
