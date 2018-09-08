a89813491c0387a4425c9fb104a9c84823348d15
omerio9 <33478787+omerio9@users.noreply.github.com>
2018-09-08 21:26:32 +0100

    Update FacultyController.scala

--- app/controllers/courses/FacultyController.scala
+++ app/controllers/courses/FacultyController.scala
@@ -1,6 +1,15 @@
 
-/**
-  *Creater Omega
-  *Reviewer Wailed
-  *
-  */
+package controllers.courses
+
+/*
+ * Created by Omega
+ * Reviewed by Walied
+ */
+
+import javax.inject.Inject
+import play.api.mvc.{AbstractController, ControllerComponents}
+
+class FacultyController @Inject()(cc: ControllerComponents) extends AbstractController(cc)  {
+
+
+}
