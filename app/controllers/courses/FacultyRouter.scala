7deb5d1d439cf56f2563f23d2648cadbfc0dcb6d
omerio9 <33478787+omerio9@users.noreply.github.com>
2018-09-08 21:33:29 +0100

    Update FacultyRouter.scala

--- app/controllers/courses/FacultyRouter.scala
+++ app/controllers/courses/FacultyRouter.scala
@@ -1,6 +1,12 @@
 
-/**
-  *Creater Omega
-  *Reviewer Wailed
-  *
-  */
+package controllers.courses
+
+import javax.inject.Inject
+import play.api.routing.Router.Routes
+import play.api.routing.SimpleRouter
+
+class FacultyRouter @Inject()
+(facultyController: FacultyController)
+  extends SimpleRouter {
+  override def routes: Routes = ???
+}
