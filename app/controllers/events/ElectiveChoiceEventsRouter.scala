98228451fa63626b28f7787494a7d62b23cb8e45
omerio9 <33478787+omerio9@users.noreply.github.com>
2018-09-08 21:00:09 +0100

    Update ElectiveChoiceEventsRouter.scala

--- app/controllers/events/ElectiveChoiceEventsRouter.scala
+++ app/controllers/events/ElectiveChoiceEventsRouter.scala
@@ -1,6 +1,19 @@
 
+
+package controllers.events
+
+import javax.inject.Inject
+import play.api.routing.Router.Routes
+import play.api.routing.SimpleRouter
+
 /**
   *Creater Omega
   *Reviewer Natasha
   *
   */
+
+class ElectiveChoiceEventsRouter @Inject()
+ (ElectiveChoiceEventsCreationRouter: ElectiveChoiceEventsController)
+  extends SimpleRouter {
+  override def routes: Routes = ???
+}
