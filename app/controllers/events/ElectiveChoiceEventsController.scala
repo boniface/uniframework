ef94b85d9a90814510c81fa24222493b9ad32aab
omerio9 <33478787+omerio9@users.noreply.github.com>
2018-09-08 20:42:15 +0100

    Update ElectiveChoiceEventsController.scala

--- app/controllers/events/ElectiveChoiceEventsController.scala
+++ app/controllers/events/ElectiveChoiceEventsController.scala
@@ -8,11 +8,7 @@ package controllers.events
 import javax.inject.Inject
 import play.api.mvc.{AbstractController, ControllerComponents}
 
-/**
-  *Creater Natasha
-  *Reviewer Omega
-  *
-  */
+
 
 class ElectivChoicesEventsController @Inject()
 (cc: ControllerComponents) extends AbstractController(cc) {
