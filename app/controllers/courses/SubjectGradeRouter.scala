6f1983744dbb41f2287ab5b36d749df0250d4e9a
Walied12 <33478150+Walied12@users.noreply.github.com>
2018-09-09 18:44:51 +0200

    Update SubjectGradeRouter.scala

--- app/controllers/courses/SubjectGradeRouter.scala
+++ app/controllers/courses/SubjectGradeRouter.scala
@@ -14,7 +14,7 @@ import play.api.routing.SimpleRouter
   * Created by hashcode on 2017/03/04.
   */
 class SubjectGradeRouterRouter @Inject()
- (subjectGradeRouterCreationRouter: SubjectGradeController)
+ (subjectGradeCreationRouter: SubjectGradeController)
   extends SimpleRouter {
   override def routes: Routes = ???
 }
