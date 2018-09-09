93fe4e44803bfc0d7052b419830dc1afe0e44539
Walied12 <33478150+Walied12@users.noreply.github.com>
2018-09-09 18:33:39 +0200

    Update StaffController.scala

--- app/controllers/people/StaffController.scala
+++ app/controllers/people/StaffController.scala
@@ -1,6 +1,21 @@
+package controllers.people
 
 /**
   *Creater Wailed
   *Reviewer Kessel
   *
   */
+
+
+import javax.inject.Inject
+import play.api.mvc.{AbstractController, ControllerComponents}
+
+/**
+  * Created by hashcode on 2017/03/04.
+  */
+
+class StaffController @Inject()
+(cc: ControllerComponents) extends AbstractController(cc) {
+
+
+}
