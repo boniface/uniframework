3c844a1c13b5666f48e0efcac4a46c6fccc0b79d
Walied12 <33478150+Walied12@users.noreply.github.com>
2018-09-09 18:41:35 +0200

    Update SubjectGradeRouter.scala

--- app/controllers/courses/SubjectGradeRouter.scala
+++ app/controllers/courses/SubjectGradeRouter.scala
@@ -1,5 +1,20 @@
+package controllers.courses
+
 /**
   *Creater Wailed
   *Reviewer Thomas
   *
   */
+
+import javax.inject.Inject
+import play.api.routing.Router.Routes
+import play.api.routing.SimpleRouter
+
+/**
+  * Created by hashcode on 2017/03/04.
+  */
+class SubjectGradeRouterRouter @Inject()
+ (subjectGradeRouterCreationRouter: SubjectGradeController)
+  extends SimpleRouter {
+  override def routes: Routes = ???
+}
