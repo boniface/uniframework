8f30236be1ea4e4d5f98879f701aae16714a9335
Kessel <33414897+Koumou@users.noreply.github.com>
2018-09-10 11:12:07 +0200

    Update StaffRouter.scala

--- app/controllers/people/StaffRouter.scala
+++ app/controllers/people/StaffRouter.scala
@@ -10,10 +10,6 @@ import javax.inject.Inject
 import play.api.routing.Router.Routes
 import play.api.routing.SimpleRouter
 
-/**
-  * Created by hashcode on 2017/03/04.
-  */
-
 class StaffRouter @Inject()
  (staffCreationRouter: StaffController)
   extends SimpleRouter {
