db8dcd552319b596497e88b34f1472dc63efe92b
Kessel <33414897+Koumou@users.noreply.github.com>
2018-09-10 11:07:22 +0200

    Update StaffSubjectsController.scala

--- app/controllers/courses/StaffSubjectsController.scala
+++ app/controllers/courses/StaffSubjectsController.scala
@@ -2,16 +2,12 @@ package controllers.courses
 
 /**
   *Creater Wailed
-  *Reviewer Thomas
+  *Reviewer Kessel
   *
   */
 
 import javax.inject.Inject
 import play.api.mvc.{AbstractController, ControllerComponents}
-
-/**
-  * Created by hashcode on 2017/03/04.
-  */
 
 class StaffSubjectsController @Inject()
 (cc: ControllerComponents) extends AbstractController(cc) {
