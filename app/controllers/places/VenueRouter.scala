a89b6f186fb97c1e71c4452f02006b5b492b5964
Queengamer <33478834+Queengamer@users.noreply.github.com>
2018-09-10 09:07:17 +0200

    Update VenueRouter.scala

--- app/controllers/places/VenueRouter.scala
+++ app/controllers/places/VenueRouter.scala
@@ -5,12 +5,12 @@ import play.api.routing.Router.Routes
 import play.api.routing.SimpleRouter
 
 /**
-  *Created by Yandisa Katiya
-  *Reviewer Kessel
-  *
+  * Created by Yandisa Katiya
+  * Reviewer Natasha
   */
 
-class VenueRouter @Inject()(venueCreationRouter: VenueController) extends SimpleRouter
-{
+class VenueRouter @Inject()
+ (venueCreationRouter: VenueController) 
+  extends SimpleRouter {
   override def routes: Routes = ???
 }
