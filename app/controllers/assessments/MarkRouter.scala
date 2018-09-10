69d0042a2be39abf35320c7112a36da9adf00042
Kessel <33414897+Koumou@users.noreply.github.com>
2018-09-10 11:13:51 +0200

    Update MarkRouter.scala

--- app/controllers/assessments/MarkRouter.scala
+++ app/controllers/assessments/MarkRouter.scala
@@ -10,7 +10,6 @@ import javax.inject.Inject
 import play.api.routing.Router.Routes
 import play.api.routing.SimpleRouter
 
-
 class MarkRouter @Inject()
  (markCreationRouter: MarkController)
   extends SimpleRouter {
