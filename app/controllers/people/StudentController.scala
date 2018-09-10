5b931e8c016fc5185f4bbfad0dcde63c58a68af4
Kessel <33414897+Koumou@users.noreply.github.com>
2018-09-10 11:12:57 +0200

    Update StudentController.scala

--- app/controllers/people/StudentController.scala
+++ app/controllers/people/StudentController.scala
@@ -10,8 +10,6 @@ package controllers.people
 import javax.inject.Inject
 import play.api.mvc.{AbstractController, ControllerComponents}
 
-
-
 class StudentController @Inject()
 (cc: ControllerComponents) extends AbstractController(cc) {
 
