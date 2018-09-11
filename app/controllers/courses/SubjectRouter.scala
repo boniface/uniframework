7c40adf69541decb42877d9d75610e8cae3b06fe
Dorcas Mbi <24913040+dorcasmbi@users.noreply.github.com>
2018-09-11 10:00:30 +0200

    Update SubjectRouter.scala

--- app/controllers/courses/SubjectRouter.scala
+++ app/controllers/courses/SubjectRouter.scala
@@ -4,3 +4,16 @@
   *Reviewer Omega
   *
   */
+package controllers.courses
+
+import javax.inject.Inject
+import play.api.routing.Router.Routes
+import play.api.routing.SimpleRouter
+
+
+class SubjectRouter @Inject()
+ (ExamCreationRouter: SubjectController)
+  extends SimpleRouter {
+  override def routes: Routes = ???
+}
+
