23d72b7e6099b193d5d6dac8acf4470f2a7b2c0a
Dorcas Mbi <24913040+dorcasmbi@users.noreply.github.com>
2018-09-11 09:51:37 +0200

    Update ExamController.scala

--- app/controllers/assessments/ExamController.scala
+++ app/controllers/assessments/ExamController.scala
@@ -1,8 +1,17 @@
 
-
-
 /**
   * Created Dorcas on 2018/07/18
   *Reviewer Kessel
   *
   */
+package controllers.assessments
+
+import javax.inject.Inject
+import play.api.mvc.{AbstractController, ControllerComponents}
+
+
+class ExamController @Inject()
+(cc: ControllerComponents) extends AbstractController(cc) {
+
+
+}
