f5007d0d71f17adfad94eb30e4e3b1639d1af3af
Dorcas Mbi <24913040+dorcasmbi@users.noreply.github.com>
2018-09-11 09:59:36 +0200

    Update SubjectController.scala

--- app/controllers/courses/SubjectController.scala
+++ app/controllers/courses/SubjectController.scala
@@ -4,3 +4,15 @@
   *Reviewer Omega
   *
   */
+package controllers.courses
+
+import javax.inject.Inject
+import play.api.mvc.{AbstractController, ControllerComponents}
+
+
+class SubjectController @Inject()
+(cc: ControllerComponents) extends AbstractController(cc) {
+
+
+}
+
