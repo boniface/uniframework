6811b07278d08effdc2f53d7c5111ab00fe4ea67
Reece1994 <33478750+Reece1994@users.noreply.github.com>
2018-09-24 00:06:36 +0200

    Update PersonTypesRouter.scala

--- app/controllers/people/PersonTypesRouter.scala
+++ app/controllers/people/PersonTypesRouter.scala
@@ -1,5 +1,11 @@
-/**
-  *Creater Reece
-  *Reviewer Thomas
-  *
-  */
+package controllers.people
+
+import javax.inject.Inject
+import play.api.routing.Router.Routes
+import play.api.routing.SimpleRouter
+
+class StaffRouter @Inject()
+ (peopleTypesCreationRouter: PeopleTypesController)
+  extends SimpleRouter {
+  override def routes: Routes = ???
+}
