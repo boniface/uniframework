59e689ff0fe157f0de2688f8158a1bde696bd504
Reece1994 <33478750+Reece1994@users.noreply.github.com>
2018-09-24 00:09:17 +0200

    Update and rename DepRouter.scala to DepartmentRouter.scala

--- app/controllers/department/DepartmentRouter.scala
+++ app/controllers/department/DepartmentRouter.scala
@@ -4,8 +4,8 @@ import javax.inject.Inject
 import play.api.routing.Router.Routes
 import play.api.routing.SimpleRouter
 
-class DepRouter @Inject()
-(depController: DepController)
+class DepartmentRouter @Inject()
+(departmentController: DepartmentController)
   extends SimpleRouter {
   override def routes: Routes = ???
 }
